8487d06966dfe55f0707c31e88a148bf6e592f08
Mourat <mourat@boutry.me>
2023-10-03 00:36:23 +0200

    chore: migrations, seeders
    Added migrations and seeders.

--- database/seeders/ProductionSeeder.php
+++ database/seeders/ProductionSeeder.php
@@ -10,26 +10,15 @@ use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
-class DatabaseSeeder extends Seeder
+class ProductionSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        Role::factory()->create(['title' => 'Author', 'slug' => 'author']);
-        User::factory(rand(3, 8))->create();
-
-        $categories = Category::factory(rand(5, 10))->create();
-
-        Post::factory(rand(10, 20))
-            ->create()
-            ->each(fn(Post $post) => $post
-                ->categories()
-                ->sync($categories->random(rand(1, 5)))
-            );
-
         Role::factory()->create(['title' => 'User', 'slug' => 'user']);
+        Role::factory()->create(['title' => 'Author', 'slug' => 'author']);
         Role::factory()->create(['title' => 'Admin', 'slug' => 'admin']);
 
         User::factory()->create([
